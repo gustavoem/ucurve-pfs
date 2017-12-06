eba2c402d6563f6b0d6e6b2c9104bada9a8160d9
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2017-12-05 23:35:18 -0200

    Updated presentation to be able to present it in 18 min more or less

--- bin/experiments/plot_time.py
+++ bin/experiments/plot_time.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-my_colors = ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3"]
+my_colors = ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854"]
 alg_color = {"PUCS": my_colors[0],
              "SFFS": my_colors[1],
              "BFS": my_colors[2],
@@ -9,24 +9,28 @@ alg_color = {"PUCS": my_colors[0],
              "PFS": my_colors[0],
              "UBB": my_colors[1],
              "PPFS": my_colors[2],
-             "RPFS": my_colors[3],
-             "UBB-PFS": my_colors[2]}
+             "OPFS": my_colors[3],
+             "UBB-PFS": my_colors[2],
+             "PFS-RAND": my_colors[4]}
 
 # file_name = "sffs_bfs_pucs_times.txt"
 # file_name = "rpfs_pfs_times.txt"
 # file_name = "ppfs_pfs_times.txt"
 file_name = "ubb_pfs_ubb-pfs_times.txt"
+# file_name = "pfs_rand_times.txt"
+
 
 
 n = []
 sffs_times = [[], []]
 bfs_times = [[], []]
 pucs_times = [[], []]
-pfs_times = [[],[]]
-rpfs_times = [[],[]]
-ppfs_times = [[],[]]
-ubb_times = [[],[]]
-ubbpfs_times = [[],[]]
+pfs_times = [[], []]
+rpfs_times = [[], []]
+ppfs_times = [[], []]
+ubb_times = [[], []]
+ubbpfs_times = [[], []]
+pfs_rand_times = [[], []]
 
 # n   SFFS            BFS             PUCS
 # 0   1     2 3       4     5 6       7     8 9 
@@ -35,7 +39,7 @@ ubbpfs_times = [[],[]]
 # 33  0.014 ∓ 0.007   0.007 ∓ 0.001   0.570 ∓ 0.538
 # 34  0.014 ∓ 0.008   0.007 ∓ 0.001   0.440 ∓ 0.424
 
-# n   PFS             RPFS
+# n   PFS             OPFS
 # 1   0.006 ∓ 0.006   0.005 ∓ 0.004 
 # 2   0.005 ∓ 0.003   0.005 ∓ 0.002 
 # 3   0.005 ∓ 0.003   0.005 ∓ 0.002 
@@ -78,21 +82,28 @@ with open(file_name) as f:
         pfs_times[0].append (float (v[4]))
         pfs_times[1].append (float (v[6]))
         ubbpfs_times[0].append (float (v[7]))
-        ubbpfs_times[1].append (float (v[9]))        
+        ubbpfs_times[1].append (float (v[9]))   
+
+        # pfs_times[0].append (float (v[1]))
+        # pfs_times[1].append (float (v[3]))
+        # pfs_rand_times[0].append (float (v[4]))
+        # pfs_rand_times[1].append (float (v[6]))     
 
 results = {}
 # algorithms = ["SFFS", "BFS", "PUCS"]
-# algorithms = ["PFS", "RPFS"]
+# algorithms = ["PFS", "OPFS"]
 # algorithms = ["PFS", "PPFS"]
 algorithms = ["UBB", "PFS", "UBB-PFS"]
+# algorithms = ["PFS", "PFS-RAND"]
 results["SFFS"] = sffs_times
 results["BFS"] = bfs_times
 results["PUCS"] = pucs_times
 results["PFS"] = pfs_times
-results["RPFS"] = rpfs_times
+results["OPFS"] = rpfs_times
 results["PPFS"] = ppfs_times
 results["UBB"] = ubb_times
 results["UBB-PFS"] = ubbpfs_times
+results["PFS-RAND"] = pfs_rand_times
 
 # Plot data
 ax = plt.gca ()
@@ -108,7 +119,8 @@ plt.ylabel ("Tempo médio de execução")
 # plt.ylabel ("Average execution time")
 plt.title ("")
 
-# plt.savefig ('rpfs_avg_time.pdf', bbox_inches='tight', transparent=True)
+# plt.savefig ('opfs_avg_time.pdf', bbox_inches='tight', transparent=True)
 # plt.savefig ('ppfs_avg_time.pdf', bbox_inches='tight', transparent=True)
 # plt.savefig ('pucs_avg_time.pdf', bbox_inches='tight', transparent=True)
 plt.savefig ('ubb-pfs_avg_time.pdf', bbox_inches='tight', transparent=True)
+# plt.savefig ('pfs_rand_avg_time.pdf', bbox_inches='tight', transparent=True)
